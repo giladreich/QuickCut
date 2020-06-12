3e81343efd3f96f45da90f7ebf06720f6aa70cf8
Gilad Reich <giladreichgr@gmail.com>
2020-06-12 11:57:44 +0200

    Implement script to format project using clang-format within bash.

---
+++ contrib/run-clang-format.sh
@@ -0,0 +1,11 @@
+# Make sure LLVM is installed and the bin directory is set in PATH.
+# Run the following command from the root directory of the project within bash terminal.
+# ./contrib/run-clang-format.sh
+
+find \
+    source/QuickCut \
+    source/QuickCutConsole \
+    source/QuickCutService \
+    source/QuickCutShared \
+    -regex '.*\.\(cpp\|h\|hpp\|cc\|cxx\)' -exec \
+    clang-format -style=file -i {} \;
