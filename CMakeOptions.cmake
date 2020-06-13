b165aca51a8339d779ac14c8f158b1091a5352d2
Gilad Reich <giladreichgr@gmail.com>
2020-06-13 07:45:05 +0200

    Enable usage of conan by default.

--- CMakeOptions.cmake
+++ CMakeOptions.cmake
@@ -3,4 +3,4 @@ cmake_minimum_required(VERSION 3.12)
 
 option(USE_CLANG_FORMAT "Set to ON if you want to generate clang-format custom target." OFF)
 
-option(USE_CONAN_PM     "Set to ON if you want to use conan package manager."           OFF)
+option(USE_CONAN_PM     "Set to ON if you want to use conan package manager."           ON)
