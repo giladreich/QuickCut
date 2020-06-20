0925d428593f674f14ae6bb8412fe26f7cf4c921
Gilad Reich <giladreichgr@gmail.com>
2020-06-20 19:54:03 +0200

    Update CMake required version to 3.16.
    + Remove cmake version required from regular directories.

--- CMakeOptions.cmake
+++ CMakeOptions.cmake
@@ -1,6 +1,4 @@
 
-cmake_minimum_required(VERSION 3.12)
-
 option(USE_CLANG_FORMAT "Set to ON if you want to generate clang-format custom target." OFF)
 
 option(USE_CONAN_PM     "Set to ON if you want to use conan package manager."           ON)
