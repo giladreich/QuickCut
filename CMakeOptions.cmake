e4be7bf0ba540cf6eb9b50734f84460e2f41a189
Gilad Reich <giladreichgr@gmail.com>
2020-06-22 07:03:37 +0200

    Implement ability to generate Doxygen code documentation using cmake.

--- CMakeOptions.cmake
+++ CMakeOptions.cmake
@@ -2,3 +2,5 @@
 option(USE_CLANG_FORMAT "Set to ON if you want to generate clang-format custom target." OFF)
 
 option(USE_CONAN_PM     "Set to ON if you want to use conan package manager."           ON)
+
+option(GENERATE_DOCS    "Set to ON if you want to generate code documentation."         OFF)
