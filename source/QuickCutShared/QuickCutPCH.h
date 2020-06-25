5deede9baed108e03f486bf43a2b8a4d37e92ad4
Gilad Reich <giladreichgr@gmail.com>
2020-06-25 16:27:16 +0200

    Move QuickCut definitions from Types to PCH.

--- source/QuickCutShared/QuickCutPCH.h
+++ source/QuickCutShared/QuickCutPCH.h
@@ -31,3 +31,20 @@
 #    define WIN32_LEAN_AND_MEAN
 #    include <Windows.h>
 #endif
+
+// Defines
+#define QUICKCUT_VERSION     "2.0.0"
+#define QUICKCUT_IPC         "QuickCut_IPC_" QUICKCUT_VERSION
+#define QUICKCUT_NAME        "QuickCut"
+#define QUICKCUTCONSOLE_NAME "QuickCutConsole"
+#define QUICKCUTSERVICE_NAME "QuickCutService"
+
+#if defined(Q_OS_WIN)
+#    define QUICKCUT_BIN        QUICKCUT_NAME ".exe"
+#    define QUICKCUTCONSOLE_BIN QUICKCUTCONSOLE_NAME ".exe"
+#    define QUICKCUTSERVICE_BIN QUICKCUTSERVICE_NAME ".exe"
+#else
+#    define QUICKCUT_BIN        QUICKCUT_NAME
+#    define QUICKCUTCONSOLE_BIN QUICKCUTCONSOLE_NAME
+#    define QUICKCUTSERVICE_BIN QUICKCUTSERVICE_NAME
+#endif
