0906fa708e307d9bec120b63be20a7d72f83e272
Gilad Reich <giladreichgr@gmail.com>
2020-07-11 16:50:17 +0200

    Update workflows to generate and upload installers for x86/x64 builds.

--- source/QuickCutInstaller/Windows/x86/create.bat
+++ source/QuickCutInstaller/Windows/x86/create.bat
@@ -18,6 +18,13 @@ xcopy /s /y ..\..\..\..\docs\LICENSE %META_PATH%
 xcopy /s /y ..\..\..\..\build\bin\QuickCut* %DATA_PATH%
 xcopy /s /y ..\..\..\..\contrib\service\* %DATA_PATH%scripts\
 windeployqt %DATA_PATH%
+
+REM For some odd reason, windeployqt on the ci vm's doesn't deploy vc redists.
+if not exist "%DATA_PATH%vc_redist.%ARCH%.exe" (
+    echo "Downloading redistributables from Microsoft servers..."
+    curl -L "https://aka.ms/vs/16/release/vc_redist.%ARCH%.exe" -o "%DATA_PATH%vc_redist.%ARCH%.exe"
+)
+
 binarycreator -v -f -r resources/installer.qrc -c config/config.xml -p packages %OUTPUT%
 
 cd resources
